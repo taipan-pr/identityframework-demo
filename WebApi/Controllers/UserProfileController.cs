3ec246588ba4134ec45be73bb4da910ce3ea96e6
Taipan Prasithpongchai <dewnoibkk@gmail.com>
2017-05-18 11:44:18 +0700

    Initialize Project
    - Install NuGet packages
    - Setup OWIN pipeline
    - Setup DI (Autofac)
    - Create Controller to test OWIN pipeline

---
+++ WebApi/Controllers/UserProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("User")]
+    public class UserProfileController : ApiController
+    {
+        [HttpGet]
+        [Route]
+        public HttpResponseMessage TestResponseMessage()
+        {
+            return this.Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Web.Compilation;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            var builder = new ContainerBuilder();
+
+            var referencedAssemblies = BuildManager.GetReferencedAssemblies().Cast<Assembly>();
+            builder.RegisterAssemblyModules(referencedAssemblies.ToArray());
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            app.UseWebApi(config);
+        }
+    }
+}
