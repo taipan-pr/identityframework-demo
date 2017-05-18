0346506612280a7238c2cb96faac82e96a501d8c
Taipan Prasithpongchai <dewnoibkk@gmail.com>
2017-05-18 11:51:26 +0700

    Initialize Identity Framework
    - Install Identity Framework package from NuGet
    - Setup Identity Framework architecture
    - Create UserProfile model

---
+++ WebApi/Identity/Managers/UserManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using WebApi.Models;
+
+namespace WebApi.Identity.Managers
+{
+    internal class UserManager : UserManager<UserProfile, Guid>, IUserManager
+    {
+        public UserManager(IUserStore<UserProfile, Guid> store) : base(store)
+        {
+        }
+    }
+}
